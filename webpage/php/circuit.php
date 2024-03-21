4e28daad764aef003eaaa81707165c87bbc2c8d1
Kasper <kasperjnssen@gmail.com>
2024-03-21 17:18:36 +0100

    added circuit info

--- webpage/php/circuit.php
+++ webpage/php/circuit.php
@@ -1,1 +1,34 @@
 <?php
+include 'getMySqli.php';
+
+$mysqli = getMysqli();
+
+// Retrieve the driver name from the POST request
+$circuit = $mysqli->real_escape_string($_SESSION['search']);
+
+$sql = "SELECT * FROM circuit_View WHERE `circuit naam` = '$circuit'";
+$result = $mysqli->query($sql);
+error_log("Number of rows: " . $result->num_rows); // Add this line
+
+if ($mysqli->error) {
+    error_log("SQL error: " . $mysqli->error); // Log the error
+    http_response_code(500); // Send a 500 status code
+    echo "SQL error: " . $mysqli->error;
+    exit;
+}
+
+if ($result->num_rows > 0) {
+    // Fetch the first row
+    $row = $result->fetch_assoc();
+
+    foreach ($row as $key => $value) {
+        if (is_null($value)) {
+            $row[$key] = "N/A"; // replace with default value
+        }
+    }
+
+//    $row['selfie'] = base64_encode($row['selfie']);
+    // Return circuit information as JSON
+    header('Content-Type: application/json');
+    echo json_encode($row);
+}
