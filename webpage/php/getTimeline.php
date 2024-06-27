915847bc5f66953c6853be700e3e8779922bfdb2
Flits <kasperjnssen@gmail.com>
2024-06-27 11:54:09 +0200

    Started on circuitOverview

---
+++ webpage/php/getTimeline.php
@@ -0,0 +1,29 @@
+<?php
+include "getMySqli.php";
+
+$mysqli = getMysqli();
+
+$query = "CALL getCircuitOverview()";
+
+error_log("Executing query: " . $query);
+
+$result = $mysqli->query($query);
+error_log("Number of rows: " . $result->num_rows);
+
+if ($mysqli->error) {
+    error_log("SQL error: " . $mysqli->error); // Log the error
+    http_response_code(500); // Send a 500 status code
+    echo "SQL error: " . $mysqli->error;
+    exit;
+}
+
+$products = array();
+
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        array_push($products, $row);
+        error_log("Row: " . print_r($row, true));
+    }
+}
+
+return $products;
