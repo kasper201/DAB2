8d79180092dddd01a1f26fcf77b83712d0163dd0
Kasper <kasperjnssen@gmail.com>
2024-07-05 00:30:29 +0200

    corrected spacing

--- webpage/php/utf8ize.php
+++ webpage/php/utf8ize.php
@@ -1,11 +1,11 @@
 <?php
 function utf8ize($mixed) {
-if (is_array($mixed)) {
-foreach ($mixed as $key => $value) {
-$mixed[$key] = utf8ize($value);
-}
-} else if (is_string($mixed)) {
-return utf8_encode($mixed);
-}
-return $mixed;
+    if (is_array($mixed)) {
+        foreach ($mixed as $key => $value) {
+            $mixed[$key] = utf8ize($value);
+        }
+    } else if (is_string($mixed)) {
+        return utf8_encode($mixed);
+    }
+    return $mixed;
 }
