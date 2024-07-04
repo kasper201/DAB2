ce236f6c216c76d22ef4217d039c0892d4f91a1f
Kasper <kasperjnssen@gmail.com>
2024-07-05 00:28:50 +0200

    Added utf8size.php to be able to load driverOverview.php

--- webpage/php/driverOverview.php
+++ webpage/php/driverOverview.php
@@ -1,13 +1,22 @@
 <?php
 include "getMySqli.php";
 include "getArr.php";
+include "utf8ize.php";
+
 
 $mysqli = getMysqli();
-
-$query = "SELECT * FROM coureur_view ORDER BY naam DESC;";
-
+$query = "SELECT * FROM coureur_view ORDER BY naam ASC;";
 $drivers = getArr($query, $mysqli);
 
-header('Content-Type: application/json');  // Set the Content-Type header
-echo json_encode($drivers);
-return $drivers;
+header('Content-Type: application/json');
+
+$drivers = utf8ize($drivers);
+$json = json_encode($drivers);
+if ($json === false) {
+    error_log("JSON encoding error: " . json_last_error_msg());
+    http_response_code(500);
+    echo json_encode(["error" => "JSON encoding error: " . json_last_error_msg()]);
+    exit;
+}
+
+echo $json;
